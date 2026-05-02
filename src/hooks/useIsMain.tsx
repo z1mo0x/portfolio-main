e1956266262ee5c7c69cb0aee05d5fd4fd5cd108
z1mo0x <droweckiy@mail.ru>
2026-05-02 19:02:52 +0300

    adaptive-main-page

--- src/hooks/useIsMain.tsx
+++ src/hooks/useIsMain.tsx
@@ -1,18 +1,15 @@
 'use client'
 
 import { usePathname } from "next/navigation";
-import { useLayoutEffect, useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function useIsMain() {
     const pathname = usePathname()
     const [isMain, setIsMain] = useState<boolean>(false);
 
-    useLayoutEffect(() => {
+    useEffect(() => {
         setIsMain(pathname === '/')
     }, [pathname])
-
-    console.log(isMain);
-
 
     return isMain;
 }
