0af19f8833cb00acb7b607b048fdaba93970a348
z1mo0x <droweckiy@mail.ru>
2026-05-02 19:33:39 +0300

    adaptive-main-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,25 @@
-import Skills from "@/components/ui/skills/skills";
 import Hero from "@/components/ui/hero";
-import ProjectsBlock from "@/components/ui/projects/projects-block";
 import AboutBlock from "@/components/ui/about/about-block";
-import ReviewsBlock from "@/components/ui/reviews/reviews-block";
-import BlogBlock from "@/components/ui/blog/blog-block";
 import dynamic from "next/dynamic";
 
 
+const HeroLazy = dynamic(() => import('@/components/ui/hero'))
 const ServicesBlockLazy = dynamic(() => import('@/components/lazy/services-block'))
+const SkillsLazy = dynamic(() => import('@/components/ui/skills/skills'))
+const ProjectsBlockLazy = dynamic(() => import('@/components/ui/projects/projects-block'))
+const ReviewsBlockLazy = dynamic(() => import('@/components/ui/reviews/reviews-block'))
+const BlogBlockLazy = dynamic(() => import('@/components/ui/blog/blog-block'))
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      <HeroLazy />
       <AboutBlock />
       <ServicesBlockLazy />
-      <Skills />
-      <ProjectsBlock />
-      <ReviewsBlock />
-      <BlogBlock headingLevel={2} />
+      <SkillsLazy />
+      <ProjectsBlockLazy />
+      <ReviewsBlockLazy />
+      <BlogBlockLazy headingLevel={2} />
     </>
 
   );
